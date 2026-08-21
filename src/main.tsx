b0b4b64bf05b828bf2480beb0d0880096f8cda27
Btech-sty <owolabiblessing075@gmail.com>
2026-08-21 13:11:14 +0100

    updated my main.jsx file

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import './App.css'
-import Land from './pages/play'
-// import App from './App.tsx'
+//import Land from './pages/play'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <Land/>
+    <App />
+    {/* <Land/> */}
   
   </StrictMode>,
 )
